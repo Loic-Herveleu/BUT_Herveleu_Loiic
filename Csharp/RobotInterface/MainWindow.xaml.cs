137beeded623eb65ed03f2f25825af504954c0ad
IUT GEII Toulon Table 6 <rct@univ-tln.fr>
2022-10-18 12:35:45 +0200

    TP2.2jusquautimer

--- Csharp/RobotInterface/MainWindow.xaml.cs
+++ Csharp/RobotInterface/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ExtendedSerialPort;
+using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace RobotInterface
 {
@@ -20,11 +23,27 @@ namespace RobotInterface
     /// </summary>
     public partial class MainWindow : Window
     {
+        ReliableSerialPort serialPort1;
         public MainWindow()
         {
             InitializeComponent();
+            serialPort1 = new ReliableSerialPort("COM3", 115200, Parity.None, 8, StopBits.One);
+            serialPort1.DataReceived += SerialPort1_DataReceived;
+            serialPort1.Open();
+            DispatcherTimer timerAffichage;
+            timerAffichage = new DispatcherTimer();
+            timerAffichage.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timerAffichage.Tick += TimerAffichage_Tick;
+            timerAffichage.Start();
+        }
+        string receivedText;
+        public void SerialPort1_DataReceived(object sender, DataReceivedArgs e)
+        {
+            receivedText += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
+
         }
 
+        string text;
         bool toggle = false;
         private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
         {
@@ -42,17 +61,8 @@ namespace RobotInterface
         }
         private void SendMessage()
         {
-            if (TextBoxEmission.Text != "" && TextBoxEmission.Text != "\r\n")
-            {
-                TextBoxReception.Text += "Reçu :";
-                TextBoxReception.Text += TextBoxEmission.Text;
-                TextBoxEmission.Text = "";
-            }
-            else
-            {
-                TextBoxEmission.Text = "";
-            }
-
+            serialPort1.WriteLine(TextBoxEmission.Text);
+            TextBoxEmission.Text = "";
         }
 
         private void TextBoxEmission_KeyUp(object sender, KeyEventArgs e)
